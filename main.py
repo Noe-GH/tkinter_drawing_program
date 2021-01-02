f8841de772c6684101d22cfbf5851daefb64247f
noegh <noe2gracida@hotmail.com>
2021-01-02 11:10:59 -0600

    Second commit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import tkinter as tk
 from tkinter import filedialog
 from tkinter import colorchooser
-# pyscreenshot permite guardar lo que se muestra en pantalla en un objeto que puede ser manejado por la librería pillow.
 import pyscreenshot
 
 
@@ -78,37 +77,7 @@ class DrawEasy():
         self.btn_new = tk.Button(self.frame_menu, image=self.img_new, bd=0, command=self.clear_cnv)
 
         self.cnv_area = tk.Canvas(self.w, bg=drawing_area_color, height=720)
-        # Cuando se desea trabajar con eventos, es necesario usar bind.
-        # Se pasa el nombre del evento en una tag.
-        #   <B1-Motion> Es para usar el movimiento del mouse.
-        # Se pasa como segundo argumento una función que recibe un evento.
-        '''
-        Lista de tags:
-        | event                 | name                  |
-        | Ctrl-c                | Control-c             |
-        | Ctrl-/                | Control-slash         |
-        | Ctrl-\                | Control-backslash     |
-        | Ctrl+(Mouse Button-1) | Control-1             |
-        | Ctrl-1                | Control-Key-1         |
-        | Enter key             | Return                |
-        |                       | Button-1              |
-        |                       | ButtonRelease-1       |
-        |                       | Home                  |
-        |                       | Up, Down, Left, Right |
-        |                       | Configure             |
-        | window exposed        | Expose                |
-        | mouse enters widget   | Enter                 |
-        | mouse leaves widget   | Leave                 |
-        |                       | Key                   |
-        |                       | Tab                   |
-        |                       | space                 |
-        |                       | BackSpace             |
-        |                       | KeyRelease-BackSpace  |
-        | any key release       | KeyRelease            |
-        | escape                | Escape                |
-        |                       | F1                    |
-        |                       | Alt-h                 |
-        '''
+
         # Bindings
         self.cnv_area.bind("<B1-Motion>", self.draw)
         self.w.bind('<Control-F2>', self.clear_cnv)
@@ -145,6 +114,7 @@ class DrawEasy():
 
         self.w.mainloop()
 
+
     def draw(self, event):
         '''
         Function that allows to draw in the canvas. It manages the type of drawing brush, its color and its size.
@@ -156,34 +126,28 @@ class DrawEasy():
         elif square_brush:
             self.cnv_area.create_rectangle(x1, y1, x2, y2, fill=drawing_color, outline = drawing_color, width=self.spinb_pen_size.get())
         elif line_brush:
-            # Se restan y suman valores a las coordenadas para lograr el efecto. Cada vez que se oprime el botón izquierdo del mouse y se mueve el mouse, se crea una línea
-            # que va desde las coordenadas del mouse -5 hasta las coordenadas del mouse +5.
             self.cnv_area.create_line(x1-5, y1-5, x2+5, y2+5, fill=drawing_color, width=self.spinb_pen_size.get())
         elif eraser_brush:
             self.cnv_area.create_oval(x1, y1, x2, y2, fill=drawing_area_color, outline = drawing_area_color, width=self.spinb_pen_size.get())
     
-    # Poner event permite que se hagan atajos. Solo se pone event y nada más. Luego, se hace el binding en la ventana.
+
     def clear_cnv(self, event):
         '''
         Function that deletes everything on the canvas.
         '''
         self.cnv_area.delete("all")
     
+
     def save_drawing(self, event):
         '''
         Function that saves the content of the drawing area with a generic save window.
         '''
-        # Se almacena en x1, y1 las coordenadas de la ventana más las cordenadas de donde comienza el canvas.
         x1 = self.w.winfo_rootx() + self.cnv_area.winfo_x() + 2
         y1 = self.w.winfo_rooty() + self.cnv_area.winfo_y() + 2
 
-        # Se almacena en x2, y2 las coordenadas de la ventana más el tamaño del canvas.
         x2 = self.w.winfo_rootx() + self.cnv_area.winfo_width() - 2
         y2 = self.w.winfo_rootx() + self.cnv_area.winfo_height()
 
-        # Los números que se suman y se restan son para que no aparezca un marco gris de la pantalla.
-
-        # Función que crea una imagen de lo que se muestra en pantalla:
         img = pyscreenshot.grab(bbox=(x1, y1, x2, y2))
         try:
             with filedialog.asksaveasfile(mode='w', defaultextension=".png", filetypes=[('.png', '*.png*'), ('.jpeg', '*.jpeg*')]) as mf:
@@ -193,7 +157,9 @@ class DrawEasy():
             pass
     
     def color_picker(self):
-        # Función que permite utilizar un selector de colores ya prefabricado:
+        '''
+        Function that allows the user to choose a color with a color choosing dialog window.
+        '''
         color = colorchooser.askcolor()
         select_drawing_color(color[1])
 
